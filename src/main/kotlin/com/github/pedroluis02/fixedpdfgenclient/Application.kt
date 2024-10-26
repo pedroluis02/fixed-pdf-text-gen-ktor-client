d333a13c5301fc66e69c32048915fefec2966e54
Pedro Luis <pedroluisnr22@gmail.com>
2024-10-25 22:12:14 -0500

    feat: :sparkles: fetch server info

--- src/main/kotlin/com/github/pedroluis02/fixedpdfgenclient/Application.kt
+++ src/main/kotlin/com/github/pedroluis02/fixedpdfgenclient/Application.kt
@@ -1,5 +1,25 @@
 package com.github.pedroluis02.fixedpdfgenclient
 
-fun main() {
-    println("Hello World!")
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+suspend fun main() {
+    val client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                ignoreUnknownKeys = true
+            })
+        }
+    }
+
+    val response = client.get("http://0.0.0.0:8080/api/v1/info")
+    val body = response.bodyAsText()
+    println(body)
+    client.close()
 }

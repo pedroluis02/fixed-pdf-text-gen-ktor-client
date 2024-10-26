03b1beec77ac2538db84e7bc7c6bd7ed284fc887
Pedro Luis <pedroluisnr22@gmail.com>
2024-10-25 22:11:32 -0500

    build: :wrench: setup build and dependencies
    use version catalog for dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.23"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 group = "com.github.pedroluis02"
@@ -10,6 +11,12 @@ repositories {
 }
 
 dependencies {
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.logback.classic)
+
     testImplementation(kotlin("test"))
 }
 
